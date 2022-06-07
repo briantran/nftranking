45877f356380fe962ad5059004f917a6ba728434
Brian Tran <briantran86@gmail.com>
2022-06-06 23:12:46 -0700

    move some sql statements into constants

--- src/penguin_data/penguin_data.py
+++ src/penguin_data/penguin_data.py
@@ -9,6 +9,12 @@ from src.utils import chunks, flush_buffer, row_count
 
 logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
 
+DROP_TABLE_STATEMENT = f'DROP TABLE IF EXISTS {PENGUIN_TABLE_NAME}'
+CREATE_TABLE_STATEMENT = f'CREATE TABLE IF NOT EXISTS {PENGUIN_TABLE_NAME} (token int primary key, background varchar, skin varchar, body varchar, face varchar, head varchar)'
+
+PENGUIN_FEATURE_STRING = ', '.join(Penguin.FEATURES)
+INSERT_STATEMENT = f'INSERT INTO {PENGUIN_TABLE_NAME} (token, {PENGUIN_FEATURE_STRING}) VALUES (?, ?, ?, ?, ?, ?)'
+
 
 async def _fetch_all_token_metadata(tokens):
     sem = asyncio.Semaphore(DATA_FETCH_SEMAPHORE_VALUE)
@@ -30,27 +36,24 @@ async def _fetch_all_token_metadata(tokens):
 def populate_penguin_data_table(con, batch_size, refresh_penguin_data=False):
     with con:
         if refresh_penguin_data:
-            con.execute(f'DROP TABLE IF EXISTS {PENGUIN_TABLE_NAME}')
+            con.execute(DROP_TABLE_STATEMENT)
 
-        con.execute(f'CREATE TABLE IF NOT EXISTS {PENGUIN_TABLE_NAME} (token int primary key, background varchar, skin varchar, body varchar, face varchar, head varchar)')
+        con.execute(CREATE_TABLE_STATEMENT)
 
     penguin_data_row_count = row_count(con, PENGUIN_TABLE_NAME)
     missing_data = penguin_data_row_count < PENGUIN_COLLECTION_SIZE
 
     # Fetch missing NFT data
     if missing_data:
-        already_stored_tokens = set(row_data[0] for row_data in con.execute('SELECT token FROM penguins'))
+        already_stored_tokens = set(row_data[0] for row_data in con.execute(f'SELECT token FROM {PENGUIN_TABLE_NAME}'))
         tokens_to_fetch = (token for token in range(PENGUIN_COLLECTION_SIZE) if token not in already_stored_tokens)
         insertion_buffer = []
-
-        feature_string = ', '.join(Penguin.FEATURES)
-        insert_statement = f'INSERT INTO {PENGUIN_TABLE_NAME} (token, {feature_string}) VALUES (?, ?, ?, ?, ?, ?)'
 
         for chunk in chunks(tokens_to_fetch, batch_size):
             insertion_buffer.extend(asyncio.run(_fetch_all_token_metadata(chunk)))
 
             logging.info(f'Inserting into {PENGUIN_TABLE_NAME}: {[token for token, *_ in insertion_buffer]}')
-            flush_buffer(con, insert_statement, insertion_buffer)
+            flush_buffer(con, INSERT_STATEMENT, insertion_buffer)
 
     penguin_data_row_count = row_count(con, PENGUIN_TABLE_NAME)
     logging.info(f'We have data on {penguin_data_row_count} penguins!')
--- src/penguin_data/penguin_scoring.py
+++ src/penguin_data/penguin_scoring.py
@@ -5,14 +5,17 @@ from src.const import PENGUIN_TABLE_NAME, PENGUIN_COLLECTION_SIZE, PENGUIN_SCORE
 from src.penguin_data.dao import Penguin
 from src.utils import row_count, chunks, flush_buffer
 
+DROP_TABLE_STATEMENT = f'DROP TABLE IF EXISTS {PENGUIN_SCORE_TABLE_NAME}'
+CREATE_TABLE_STATEMENT = f'CREATE TABLE IF NOT EXISTS {PENGUIN_SCORE_TABLE_NAME} (token int primary key, statistical_score real, rarity_score real)'
+INSERT_STATEMENT = f'INSERT INTO {PENGUIN_SCORE_TABLE_NAME} (token, statistical_score, rarity_score) VALUES (?, ?, ?)'
+
 
 def populate_penguin_score_table(con, batch_size, refresh_penguin_scores):
     with con:
         if refresh_penguin_scores:
-            con.execute(f'DROP TABLE IF EXISTS {PENGUIN_SCORE_TABLE_NAME}')
+            con.execute(DROP_TABLE_STATEMENT)
 
-        con.execute(
-            f'CREATE TABLE IF NOT EXISTS {PENGUIN_SCORE_TABLE_NAME} (token int primary key, statistical_score real, rarity_score real)')
+        con.execute(CREATE_TABLE_STATEMENT)
     penguin_score_row_count = row_count(con, PENGUIN_SCORE_TABLE_NAME)
     missing_data = penguin_score_row_count < PENGUIN_COLLECTION_SIZE
     if missing_data:
@@ -30,7 +33,6 @@ def populate_penguin_score_table(con, batch_size, refresh_penguin_scores):
         unscored_penguins = con.execute(
             f'SELECT p.* FROM {PENGUIN_TABLE_NAME} p LEFT OUTER JOIN {PENGUIN_SCORE_TABLE_NAME} ps ON p.token=ps.token WHERE ps.token IS NULL')
         insertion_buffer = []
-        insert_statement = f'INSERT INTO {PENGUIN_SCORE_TABLE_NAME} (token, statistical_score, rarity_score) VALUES (?, ?, ?)'
         for chunk in chunks(unscored_penguins, batch_size):
             for unscored_penguin_row_data in chunk:
                 penguin = Penguin(*unscored_penguin_row_data)
@@ -38,7 +40,7 @@ def populate_penguin_score_table(con, batch_size, refresh_penguin_scores):
                 statistical_score = penguin.statistical_score(feature_count_dict)
                 insertion_buffer.append((penguin.token, statistical_score, rarity_score))
 
-            flush_buffer(con, insert_statement, insertion_buffer)
+            flush_buffer(con, INSERT_STATEMENT, insertion_buffer)
 
     penguin_score_row_count = row_count(con, PENGUIN_SCORE_TABLE_NAME)
     logging.info(f'We have scored {penguin_score_row_count} penguins!')
